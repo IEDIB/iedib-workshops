96bd297264e293889ab7d8c712438c912247303c
Josep Mulet Pol <pep.mulet@gmail.com>
2022-02-15 17:40:34 +0100

    reader + aos

--- src/reader.js
+++ src/reader.js
@@ -1,16 +1,19 @@
 (function(modul){
-    // Check of voice support in current browser
+    // Check of voice speak support in current browser
     var synth = window.speechSynthesis;
     var supported = synth != null && window.SpeechSynthesisUtterance != null;
-
-
+    // Check if voice recognition is supported
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    //var SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    var SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+    var supported2 = SpeechRecognition!=null  && SpeechRecognitionEvent!=null;
+    IB.supported2 = supported2; 
     var findVoice = function (lang, voices) {
         lang = (lang || "").toLowerCase();
         var k = 0;
         var voice = null;
         var len = (voices || []).length;
-        while (k < len && voice == null) {
-            console.error("Compare", voices[k], lang);
+        while (k < len && voice == null) { 
             if (voices[k].lang.toLowerCase() == lang) {
                 voice = voices[k];
             }
@@ -20,26 +23,57 @@
     };
 
     var voices_avail = [];
+    var volume_on = true;
     var onVoicesLoaded = function() {
         //Stop voices on page change
         window.addEventListener('unload', function(evt) {
             window.speechSynthesis.cancel();
         });
         voices_avail = window.speechSynthesis.getVoices();
-        //Trigger queue
+        
+        //Add a mute/play button to stop sounds
+        var mainEl = document.querySelector('.no-overflow');
+        mainEl.style.position = 'relative';
+        if(mainEl) {
+            var spanEl = document.createElement("span");
+            spanEl.style.position='absolute';
+            spanEl.style.right='10px';
+            spanEl.style.fontSize='125%';
+            spanEl.style.color='lightgray';
+            spanEl.style.top='0';
+            spanEl.style.cursor='pointer';
+            spanEl.title = "Desconnecta la veu";
+            var iconEl = document.createElement("i");
+            iconEl.classList.add('fas', 'fa-volume');
+            spanEl.append(iconEl);
+            mainEl.append(spanEl);
+            spanEl.addEventListener("click", function(evt){
+                evt.preventDefault();
+                if(volume_on) {
+                    window.speechSynthesis.cancel();
+                    iconEl.classList.remove('fa-volume');
+                    iconEl.classList.add('fa-volume-mute');
+                    spanEl.title = "Desconnecta la veu";
+                } else {
+                    iconEl.classList.add('fa-volume');
+                    iconEl.classList.remove('fa-volume-mute');
+                    spanEl.title = "Activa la veu";
+                }
+                volume_on = !volume_on;
+            });
+        }  
     };
 
     var langs = ['ca-ES', 'es-ES'];
     modul.reader = function(textMap, attempt){
         attempt = attempt || 0;
-        if(!supported) {
+        if(!supported || !volume_on) {
             return;
         }
         if(!voices_avail.length) {
             if(attempt < 3) {
                 //try latter
-                setTimeout(function(){
-                    console.log(voices_avail, "trying latter...", attempt);
+                setTimeout(function(){ 
                     modul.reader(textMap, attempt+1);
                 }, 800);
             }
@@ -47,11 +81,9 @@
         }  
 
         var voice = null;
-        var lang = null; 
-        console.error(voices_avail);
+        var lang = null;  
         for(var i=0, len=langs.length; i<len; i++) {
             lang = langs[i];
-            console.error(lang);
             voice = findVoice(langs[i], voices_avail);
             if(voice != null) {
                 break;
@@ -70,7 +102,25 @@
         utterance.voice = voice;
         window.speechSynthesis.speak(utterance); 
     };
-   
+
+    modul.speechRecognition = function(cb, lang, continuous) {
+        continuous = continuous || false;
+        if(!supported2) {
+            console.error("Speech recognition not supported");
+            cb && cb(null, 0);
+            return;
+        }
+        var recognition = new SpeechRecognition();
+        recognition.lang = lang;
+        recognition.interimResults = false;
+        recognition.maxAlternatives = 1;
+        recognition.start();
+        recognition.onresult = function(event) {
+            var res = event.results[0][0].transcript; 
+            var confidence = event.results[0][0].confidence;
+            cb && cb(res, confidence);
+        };
+    };
 
     if(supported) {
         if ((synth.getVoices() || []).length) {
